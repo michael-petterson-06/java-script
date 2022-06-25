a27ad9fb8b1a37634dd89922183b0d5d0ff84586
Michael <michaelpetterson06.com.br>
2022-06-25 20:24:08 -0300

    colocando eventos no arquivo scritp e finalizando projeto

--- projeto-tasks/js/scritp.js
+++ projeto-tasks/js/scritp.js
@@ -1,1 +1,47 @@
-console.log("michael petterson")
+function addTasks(){
+    const taskTitle = document.querySelector("#task-title").value;
+
+    //função de remover tarefas
+    function removeTask(task){
+        task.parentNode.remove(true)
+    }
+
+    //função de completar tareda
+    function completeTask(task) {
+        const taskToComplete = task.parentNode;
+        taskToComplete.classList.toggle('done'); //"Done" verifica se tiver essa classe ele tira se não tiver ele coloca.  
+    }
+ 
+    if (taskTitle) {
+        //clona template
+        const template = document.querySelector('.template');
+        const newTask = template.cloneNode(true);
+      
+        //adicionar título
+        newTask.querySelector('.task-title').textContent = taskTitle;
+        newTask.classList.remove('template');
+        newTask.classList.remove('hide');
+        const list = document.querySelector('#task-list');
+        list.appendChild(newTask);
+        document.querySelector("#task-title").value = '';
+
+        //evento de remover tarefas
+        const removeBtn = newTask.querySelector('.remove-btn').addEventListener('click',function(){
+            removeTask(this)            
+        });
+
+        //evento de completar tarefa
+        const doneBtn = newTask.querySelector('.done-btn').addEventListener('click',function(){
+            completeTask(this)
+        });
+     }
+}
+
+
+
+// evento para adicionar tarefas
+const addBtn = document.querySelector("#add-btn");
+addBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    addTasks()
+})
